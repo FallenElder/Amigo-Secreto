9ee55c2c42cc7e2b40b0c9b3ccd077b04017658f
Fallen Elder <nicolasnomelinisilva13@gmail.com>
2025-01-29 14:21:32 -0300

    Criação da função listaAmigos

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ function adicionarAmigo(){
     if (entradaAmigo !== ''){
         amigos.push(entradaAmigo);
         document.getElementById('amigo').value = '';
+        listaAmigos()
     }
     else{
         alert("Por favor, insira um nome.");
     }
 }
+
+function listaAmigos(){
+    var lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    let newLi;
+    for (let i = 0; i < amigos.length; i++){
+        newLi = document.createElement('li');
+        lista.appendChild(newLi);
+        newLi.appendChild(document.createTextNode(amigos[i]));
+    }
+} 
